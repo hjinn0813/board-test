f791971e45a8638e82152dca0e475835d22a804d
hjinn0813 <hjc3790@gmail.com>
2024-11-12 18:11:11 +0900

    deploy 설정 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from 'react-router-dom';
 import Header from './components/Header';
 import Main from './pages/Main';
 import Board from './pages/Board';
@@ -10,17 +15,19 @@ import './style/font.scss';
 function App() {
   return (
     <div className="container">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/board" element={<Board />} />
-          <Route path="/newpost" element={<Input />} />
-          <Route path="/edit/:id" element={<Input />} />
-          <Route path="/board/:id" element={<Read />} />
-          <Route path="/valid_pw/:id" element={<Password />} />
-        </Routes>
-      </Router>
+      <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/board" element={<Board />} />
+            <Route path="/newpost" element={<Input />} />
+            <Route path="/edit/:id" element={<Input />} />
+            <Route path="/board/:id" element={<Read />} />
+            <Route path="/valid_pw/:id" element={<Password />} />
+          </Routes>
+        </Router>
+      </BrowserRouter>
     </div>
   );
 }
